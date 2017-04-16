f645bb8bd9b256ee9de867abd444be62d4c183a3
exn1016 <exn1016@163.com>
2017-04-16 16:03:37 +0800

    add new project

---
+++ piccore/src/main/java/net/mithrigintama/piccenter/piccore/App.java
@@ -0,0 +1,13 @@
+package net.mithrigintama.piccenter.piccore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
