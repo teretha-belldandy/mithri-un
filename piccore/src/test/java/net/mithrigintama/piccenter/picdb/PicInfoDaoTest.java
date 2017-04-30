2549e01227bd38be9a67d48b789dc10e6218746e
exn1016 <exn1016@163.com>
2017-04-30 23:09:27 +0800

    add new files

---
+++ piccore/src/test/java/net/mithrigintama/piccenter/picdb/PicInfoDaoTest.java
@@ -0,0 +1,19 @@
+package net.mithrigintama.piccenter.picdb;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PicInfoDaoTest {
+
+	SqlSessionFactory sqlSessionFactory = null;
+
+	@Before
+	public void before() {
+	}
+
+	@Test
+	public void testInsert() {
+	}
+
+}
